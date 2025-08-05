98c30a1768c888da154315f45d31ab78de1503ec
Naman85Jain <jainnaman8591@gmail.com>
2025-08-05 12:33:24 +0530

    role based authorization

---
+++ journalApp/src/main/java/com/naman/journalApp/controller/AdminController.java
@@ -0,0 +1,36 @@
+package com.naman.journalApp.controller;
+
+import com.naman.journalApp.entity.User;
+import com.naman.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAll() {
+        List<User> all = userService.getAll();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-admin")
+    public ResponseEntity<?> createAdmin(User user) {
+        userService.saveAdmin(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
