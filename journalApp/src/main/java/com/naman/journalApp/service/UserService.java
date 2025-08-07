cbc988ee2eaf5829e2f3cbb6bf33cc355c8766e1
Naman85Jain <jainnaman8591@gmail.com>
2025-08-07 12:11:50 +0530

    slf4j

--- journalApp/src/main/java/com/naman/journalApp/service/UserService.java
+++ journalApp/src/main/java/com/naman/journalApp/service/UserService.java
@@ -3,6 +3,7 @@ package com.naman.journalApp.service;
 import com.naman.journalApp.entity.JournalEntry;
 import com.naman.journalApp.entity.User;
 import com.naman.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -21,9 +23,13 @@ public class UserService {
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public void saveNewUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+        } catch (Exception e) {
+            log.error("Error saving user", e);
+        }
     }
 
     public void saveAdmin (User user) {
